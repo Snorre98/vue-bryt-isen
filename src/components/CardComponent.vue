7f31d0313780ca4b100a8dd5be2b39e71d19db85
snorre sæther <snosaet@gmail.com>
2024-07-18 23:05:54 +0200

    added simple card component

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+
+defineProps<{
+    title?: string,
+    details?: string,
+    gradient?: string,
+
+}>()
+
+</script>
+
+<template>
+    <div class="card-wrapper">
+        <div class="card-header">
+            <h2 class="card-title">{{ title }}</h2>
+            <button class="card-edit-btn">Edit</button>
+        </div>
+       
+        <div class="card-gradient">{{ gradient }}</div>
+        <div class="card-details">{{ details }}</div>
+    </div>
+</template>
+
+<style scoped>
+
+.card-wrapper {
+    padding: 1rem;
+    box-shadow: 0 0 5px 2px rgba(134, 134, 134, 0.15);
+    width: 250px;
+    height: 300px;
+    max-height: 600px;
+    background-color: darkslategray;
+    border-radius: 0.15rem;
+}
+
+.card-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+}
+
+.card-edit-btn {
+    padding: 0.25rem 0.5rem 0.25rem 0.5rem;
+    background-color: rgb(255, 166, 0);
+    color: black;
+    border: none;
+    border-radius: 0.25rem;
+    cursor: pointer;
+}
+
+.card-title {
+    padding: 0;
+    margin: 0;
+}
+
+.card-details {
+    border: 2px solid black;
+    height: 40%;
+    overflow: hidden;
+}
+
+.card-gradient{
+    border: 2px solid black;
+    margin: 0.2rem;
+    height: 40%;
+}
+
+</style>
---
+++ src/components/PageCardsComponent.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import CardComponent from './CardComponent.vue';
+
+
+interface cardContent {
+    title?: string,
+    details?: string,
+    gradient?: string,
+}
+
+defineProps<{cardsContent: cardContent[]}>()
+
+
+</script>
+
+<template>
+    <div class="cards-wrapper">
+        <div v-for="item in cardsContent">
+            <CardComponent 
+            :title="item.title" 
+            :details="item.details" 
+            :gradient="item.gradient"/>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+.cards-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 2rem;
+    max-width: 1280px;
+    max-height: 1080px;
+}
+
+</style>
