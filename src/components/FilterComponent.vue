1ce2bfbdbf3fb3d19eefd929abefd7fdf54ccd0b
snorre sæther <snosaet@gmail.com>
2024-07-18 20:55:59 +0200

    adds global state store to fitler component

--- src/components/FilterComponent.vue
+++ src/components/FilterComponent.vue
@@ -1,36 +1,29 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { filterStore } from '../stores/filterStore';
 
-interface filterOption {
-    label: string;
-    value: string;
-}
-const filterOptions = ref<filterOption[]>([{label:'A', value:'a'}, {label:'B', value: 'b'}]);
-
-const selectedFilter = ref<{ [key: string]: boolean }>({});
-
-const setSelected = () => {
-    selectedFilter.value = {};
-    filterOptions.value.forEach((option) => {
-        selectedFilter.value[option.value] = false;
-    })
-}
-
+/*
 watch(filterOptions, setSelected);
+*/
+
+
+/*simulation API call*/
+setTimeout(() => {
+    filterStore.filterOptions = [{label:'A', value:'a'}, {label:'B', value: 'b'}, {label:'C', value: 'c'}];
+}, 5000);
 
 </script>
 
 <template>
-    <div v-for="option in filterOptions" :key="option.label">
+    <div v-for="option in filterStore.filterOptions" :key="option.label">
         <input type="checkbox" 
         :value="option.value" 
-        v-model="selectedFilter[option.value]"
+        v-model="filterStore.selectedOptions[option.value]"
         />
         <label>{{ option.label }}</label>
     </div>
     <div>Selected:
-        <span v-for="option in filterOptions" :key="option.label">
-            <span v-if="selectedFilter[option.value]">{{ option.value }} </span>
+        <span v-for="option in filterStore.filterOptions" :key="option.label">
+            <span v-if="filterStore.selectedOptions[option.value]">{{ option.value }} </span>
         </span>
     </div>
 </template>
